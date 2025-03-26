2564578357f1b63fd8e3520fc64df1c643310eb7
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-03-26 01:09:02 +0100

    feat: Added ByteArray object kind

--- source/vm_core/object_kinds.py
+++ source/vm_core/object_kinds.py
@@ -22,3 +22,19 @@ class Symbol(Object):
             return False
 
         return (self._text == other._text) and (self._arity == other._arity)
+
+class ByteArray(Object):
+    def __init__(self, item_count):
+        super().__init__()
+
+        self._bytes = [0] * item_count
+
+    def byte_get_at(self, index):
+        return self._bytes[index]
+
+    def item_put_at(self, index, byte):
+        self._bytes[index] = byte
+
+    def get_byte_count(self):
+        return len(self._bytes)
+
