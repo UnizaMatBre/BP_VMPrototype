dc9c642536c107689589dca76d5475bd5554a89c
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-04-02 20:55:14 +0200

    feat: Added opcode mapping table

--- source/vm_core/interpreter.py
+++ source/vm_core/interpreter.py
@@ -1,4 +1,27 @@
+
+from source.vm_core import instructions
 from source.vm_core.object_kinds import VM_Proces
+
+
+def _unknown_opcode(interpreter, parameter):
+    """
+    Tells interpreter that unknown opcode was encountered
+    TODO: Implement body when implementing error handling
+    """
+    raise NotImplementedError()
+
+"""
+List that maps all possible opcode values (both valid and invalid) to specific operations.
+First we will fill it with "unknown_opcode" mapping and then we manually set mapping for each valid opcode
+"""
+OPCODE_MAPPING = [_unknown_opcode] * 0xFF
+
+OPCODE_MAPPING[instructions.Opcodes.NOOP] = lambda interpreter, parameter: interpreter._do_nothing(parameter)
+OPCODE_MAPPING[instructions.Opcodes.PUSH_MYSELF] = lambda interpreter, parameter: interpreter._do_push_myself(parameter)
+OPCODE_MAPPING[instructions.Opcodes.PUSH_LITERAL] = lambda interpreter, parameter: interpreter._do_push_literal(parameter)
+OPCODE_MAPPING[instructions.Opcodes.PULL] = lambda interpreter, parameter: interpreter._do_pull(parameter)
+OPCODE_MAPPING[instructions.Opcodes.SEND] = lambda interpreter, parameter: interpreter._do_send(parameter)
+OPCODE_MAPPING[instructions.Opcodes.RETURN_EXPLICIT] = lambda interpreter, parameter: interpreter._do_return_explicit(parameter)
 
 
 class Interpreter:
@@ -6,7 +29,6 @@ class Interpreter:
         assert isinstance(process, VM_Proces)
 
         self._my_process = process
-
 
     def _do_nothing(self, parameter):
         raise NotImplementedError()
