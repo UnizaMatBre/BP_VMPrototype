ce9d899d9adcd0b8f6be75c5342738a2855ef781
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-04-08 22:54:18 +0200

    test: Added test for correct bytecode parsing

--- tests/test_bytecode_parsing.py
+++ tests/test_bytecode_parsing.py
@@ -1,5 +1,6 @@
 import unittest
 
+from source.vm_core.bytecode_parsing import *
 from source.vm_core.object_kinds import VM_ByteArray
 
 class UniverseMockup:
@@ -8,7 +9,32 @@ class UniverseMockup:
 
 
 class BytearrayParsingTestCase(unittest.TestCase):
-    pass
+    def test_bytearray_correct(self):
+        byte_content = [0x01, 0x02, 0x03, 0x04]
+        byte_count = len(byte_content).to_bytes(8, byteorder="big", signed=True)
+        byte_list = [LiteralTags.VM_BYTEARRAY] + list(byte_count) + byte_content
+
+        deserializer = BytecodeDeserializer(universe=UniverseMockup(), byte_list=byte_list)
+
+        result = deserializer.parse_bytearray()
+
+        self.assertTrue(
+            isinstance(result, VM_ByteArray),
+            "Byte array parsing must return VM_ByteArray"
+        )
+
+        self.assertTrue(
+            result.get_byte_count() == 4,
+            "VM_ByteArray created by byte array parsing must have exactly 4 bytes"
+        )
+
+        for index in range(len(byte_content)):
+            self.assertTrue(
+                result.byte_get_at(index) == byte_content[index],
+                "VM_ByteArray created by byte array parsing must have correct bytes"
+            )
+
+
 
 if __name__ == '__main__':
     unittest.main()
