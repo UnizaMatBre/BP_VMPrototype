ca4e7554d747a5f912dc77332a39d33f82a5167c
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-04-08 22:40:29 +0200

    test: Added bytearray factory into Universe mockup

--- tests/test_bytecode_parsing.py
+++ tests/test_bytecode_parsing.py
@@ -3,7 +3,8 @@ import unittest
 from source.vm_core.object_kinds import VM_ByteArray
 
 class UniverseMockup:
-    pass
+    def new_byte_array(self, byte_count):
+        return VM_ByteArray(byte_count)
 
 
 class BytearrayParsingTestCase(unittest.TestCase):
