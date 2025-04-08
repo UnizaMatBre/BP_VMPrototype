6fc2d2190eff7d60fa691b657302b3385ac196e3
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-04-08 22:20:15 +0200

    feat: Added byte array parsing

--- source/vm_core/bytecode_parsing.py
+++ source/vm_core/bytecode_parsing.py
@@ -1,3 +1,4 @@
+from source.vm_core.object_kinds import VM_ByteArray
 
 
 class LiteralTags:
@@ -24,5 +25,24 @@ class BytecodeDeserializer:
     def _move_by(self, distance):
         self._index += distance
 
+    def parse_bytearray(self):
+        if self._get_current() != LiteralTags.VM_BYTEARRAY:
+            raise DeserializerException()
+
+        self._move_by(1)
+
+        # read size (8-bits)
+        # TODO: handle possible error of running out of the bytes to read
+        byte_count_bytes = [self._get_current() for x in range(8)]
+
+        byte_count = int.from_bytes(byte_count_bytes, byteorder="big", signed=False)
+
+        # read values into array
+        # TODO: handle possible error of running out of the bytes to read
+        new_bytearray = self._universe.new_bytearray(byte_count)
+        for index in range(byte_count):
+            new_bytearray.byte_put_at(index, self._get_current())
+
+        return new_bytearray
 
 
