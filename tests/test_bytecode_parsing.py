f883cf33ed134ca134ed104ca00f3f2a3536f873
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-04-08 22:39:35 +0200

    test: Added testing module for bytecode parsing

---
+++ tests/test_bytecode_parsing.py
@@ -0,0 +1,13 @@
+import unittest
+
+from source.vm_core.object_kinds import VM_ByteArray
+
+class UniverseMockup:
+    pass
+
+
+class BytearrayParsingTestCase(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
