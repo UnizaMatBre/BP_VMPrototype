70d3999e55ebfc3d980b66e51d8c845e4a11a12c
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-04-08 22:35:53 +0200

    feat: Added error handling into bytearray parsing

--- source/vm_core/bytecode_parsing.py
+++ source/vm_core/bytecode_parsing.py
@@ -22,26 +22,34 @@ class BytecodeDeserializer:
     def _get_current(self):
         return self._byte_list[self._index]
 
+    def _get_next_n_bytes(self, number):
+        if self._index + number > len(self._byte_list):
+            raise DeserializerException()
+
+        return ( self._byte_list[local_index] for local_index in range(self._index, self._index + number + 1) )
+
     def _move_by(self, distance):
         self._index += distance
 
     def parse_bytearray(self):
+        # TODO: Maybe errors should be more precise?
+
         if self._get_current() != LiteralTags.VM_BYTEARRAY:
             raise DeserializerException()
 
         self._move_by(1)
 
-        # read size (8-bits)
-        # TODO: handle possible error of running out of the bytes to read
-        byte_count_bytes = [self._get_current() for x in range(8)]
+        # read size (8 bytes)
+        byte_count_bytes = self._get_next_n_bytes(8)
 
         byte_count = int.from_bytes(byte_count_bytes, byteorder="big", signed=False)
 
         # read values into array
-        # TODO: handle possible error of running out of the bytes to read
         new_bytearray = self._universe.new_bytearray(byte_count)
+        new_bytearray_content = self._get_next_n_bytes(byte_count)
+
         for index in range(byte_count):
-            new_bytearray.byte_put_at(index, self._get_current())
+            new_bytearray.byte_put_at(index, next(new_bytearray_content))
 
         return new_bytearray
 
