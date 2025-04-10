d7c39c9d9926804bf34a3ecaf22d318923a066ca
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-04-10 09:37:43 +0200

    feat: Implemented slot object parsing

--- source/vm_core/bytecode_parsing.py
+++ source/vm_core/bytecode_parsing.py
@@ -1,4 +1,5 @@
 from source.vm_core.object_kinds import VM_ByteArray
+from source.vm_core.object_layout import VM_Object, SlotKind
 
 
 class LiteralTags:
@@ -130,7 +131,32 @@ class BytecodeDeserializer:
 
 
     def unchecked_parse_slot_object(self):
-        raise NotImplementedError()
+        slot_count = self._get_next_int64()
+
+        new_slot_object = VM_Object()
+
+        for counter in range(slot_count):
+            # TODO: Create proper kind deserialization
+            slot_kind_bytes = self._get_current()
+            self._move_by(1)
+
+            slot_kind = SlotKind()
+
+            if bool(slot_kind_bytes & 0b00000001):
+                slot_kind.toggleParent()
+
+            if bool(slot_kind_bytes & 0b00000010):
+                slot_kind.toggleParameter()
+
+            slot_name = self.parse_symbol()
+            slot_content = self.parse_bytes()
+
+            # raise error if slot already exists
+            slot_created = new_slot_object.add_slot(slot_name, slot_kind, slot_content)
+            if not slot_created:
+                raise DeserializerException()
+
+        return new_slot_object
 
     def parse_slot_object(self):
         self._check_tag(LiteralTags.VM_OBJECT)
