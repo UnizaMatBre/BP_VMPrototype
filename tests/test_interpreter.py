65153122d6918b88bf52b3ed2d2303127101b4a4
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-04-11 21:47:10 +0200

    test: Added all existing tests to their correct test cases and fully removed original omni-test case

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -14,163 +14,7 @@ class UniverseMockup:
     def get_none_object(self):
         return None
 
-"""
-class InstructionsTestCase(unittest.TestCase):
-    def _setup_process(self, literals_content, bytecode_content, stack_content, none_object):
-        bytecode = VM_ByteArray(len(bytecode_content))
-        for index in range(len(bytecode_content)):
-            bytecode.byte_put_at(index, bytecode_content[index])
-
-        literals = VM_ObjectArray(len(literals_content), none_object)
-        for index in range(len(literals_content)):
-            literals.item_put_at(index, literals_content[index])
-
-        stack = VM_ObjectArray(len(stack_content), none_object)
-
-        code = object_kinds.VM_Code(literals, bytecode)
-
-        method = object_kinds.VM_Object()
-        method.set_code(code)
-
-        frame = object_kinds.VM_Frame(none_object, stack, method)
-
-        for index in range(len(stack_content)):
-            if stack_content[index] is none_object:
-                break
-            frame.push_item(stack_content[index])
-
-        return SetupResult(
-            literals=literals,
-            bytecode=bytecode,
-            stack=stack,
-            frame=frame
-        )
-
-
-
-    def test_nothing_opcode(self):
-        # setup
-        setup = self._setup_process(
-            literals_content=[None],
-            stack_content=[None],
-            bytecode_content=[Opcodes.NOOP, 0x00],
-            none_object=None
-        )
-
-        # testing
-        process = object_kinds.VM_Process(None, setup.frame)
-        interpreter = Interpreter(UniverseMockup(), process)
-        interpreter.executeInstruction()
-
-        self.assertTrue(
-            all( (setup.stack.item_get_at(index) is None) for index in range(setup.stack.get_item_count()) ),
-            "When noop opcode is executed, stack must be in its original state"
-        )
-
-    def test_push_myself_opcode(self):
-        # setup
-        setup = self._setup_process(
-            literals_content=[None],
-            stack_content=[None],
-            bytecode_content=[Opcodes.PUSH_MYSELF, 0x00],
-            none_object=None
-        )
-
-        # testing
-        process = object_kinds.VM_Process(None, setup.frame)
-        interpreter = Interpreter(UniverseMockup(), process)
-        interpreter.executeInstruction()
-
-        self.assertTrue(
-            setup.stack.item_get_at(0) is process.peek_frame().getMethodActivation(),
-            "When push_myself opcode is executed, top of the stack must be running method"
-        )
-
-    def test_push_literal_opcode(self):
-        # setup
-        setup = self._setup_process(
-            literals_content=[object_kinds.VM_Symbol("to_be_cloned", 0)],
-            stack_content=[None] * 4,
-            bytecode_content=[Opcodes.PUSH_LITERAL, 0x00],
-            none_object=None
-        )
-
-        # testing
-        process = object_kinds.VM_Process(None, setup.frame)
-        interpreter = Interpreter(UniverseMockup(), process)
-        interpreter.executeInstruction()
-
-        self.assertTrue(
-            setup.stack.item_get_at(0) == setup.literals.item_get_at(0),
-            "When push_literal opcode is executed, top of the stack should be copy of literal referenced by it"
-        )
-
-    def test_return_explicit_opcode(self):
-        # setup for root frame
-        returned_object = object_kinds.VM_Symbol("to_be_returned", 0)
-
-        setup_for_root_frame = self._setup_process(
-            literals_content=[],
-            stack_content=[None] * 4,
-            bytecode_content=[],
-            none_object=None
-        )
-
-        # setup for sub method
-        setup_for_sub_frame = self._setup_process(
-            literals_content=[],
-            stack_content=[returned_object, None],
-            bytecode_content=[Opcodes.RETURN_EXPLICIT, 0x00],
-            none_object=None
-        )
-
-        # testing
-        process = object_kinds.VM_Process(None, setup_for_root_frame.frame)
-        process.push_frame(setup_for_sub_frame.frame)
-
-        interpreter = Interpreter(UniverseMockup(), process)
-        interpreter.executeInstruction()
-
-        self.assertTrue(
-            process.peek_frame() is setup_for_root_frame.frame,
-            "When return_explicit opcode is executed, the active frame must be the one that was before returning frame"
-        )
-
-        self.assertTrue(
-            setup_for_root_frame.stack.item_get_at(0) == returned_object,
-            "When return_explicit opcode is executed, the top of root stack must be the value returned from returned frame"
-        )
-
-    def test_return_explicit_root_opcode(self):
-        # setup for root frame
-        returned_object = object_kinds.VM_Symbol("to_be_returned", 0)
-
-        setup_for_root_frame = self._setup_process(
-            literals_content=[],
-            stack_content=[returned_object, None],
-            bytecode_content=[Opcodes.RETURN_EXPLICIT, 0x00],
-            none_object=None
-        )
-
-
-
-        # testing
-        process = object_kinds.VM_Process(None, setup_for_root_frame.frame)
-        interpreter = Interpreter(UniverseMockup(), process)
-        interpreter.executeInstruction()
-
-        self.assertTrue(
-            process.peek_frame() is not setup_for_root_frame.frame,
-            "When return_explicit opcode is executed in root frame, the active frame must not be the same as "
-        )
-
-        self.assertTrue(
-            process.get_ordinary_result() == returned_object,
-            "When return_explicit opcode is executed in root frame, the process result must be the return value from returning root frame"
-        )
-"""
-
-def _setup_process(self, literals_content, bytecode_content, stack_content, none_object):
+def _setup_process(literals_content, bytecode_content, stack_content, none_object):
     bytecode = VM_ByteArray(len(bytecode_content))
     for index in range(len(bytecode_content)):
         bytecode.byte_put_at(index, bytecode_content[index])
@@ -200,19 +44,130 @@ def _setup_process(self, literals_content, bytecode_content, stack_content, none
     )
 
 class InstructionNothingTestCase(unittest.TestCase):
-    pass
+    def test_nothing_opcode_correct(self):
+        # setup
+        setup = _setup_process(
+            literals_content=[None],
+            stack_content=[None],
+            bytecode_content=[Opcodes.NOOP, 0x00],
+            none_object=None
+        )
+
+        # testing
+        process = object_kinds.VM_Process(None, setup.frame)
+        interpreter = Interpreter(UniverseMockup(), process)
+        interpreter.executeInstruction()
+
+        self.assertTrue(
+            all( (setup.stack.item_get_at(index) is None) for index in range(setup.stack.get_item_count()) ),
+            "When noop opcode is executed, stack must be in its original state"
+        )
 
 class InstructionPushMyselfTestCase(unittest.TestCase):
-    pass
+    def test_push_myself_opcode(self):
+        # setup
+        setup = _setup_process(
+            literals_content=[None],
+            stack_content=[None],
+            bytecode_content=[Opcodes.PUSH_MYSELF, 0x00],
+            none_object=None
+        )
+
+        # testing
+        process = object_kinds.VM_Process(None, setup.frame)
+        interpreter = Interpreter(UniverseMockup(), process)
+        interpreter.executeInstruction()
+
+        self.assertTrue(
+            setup.stack.item_get_at(0) is process.peek_frame().getMethodActivation(),
+            "When push_myself opcode is executed, top of the stack must be running method"
+        )
 
 class InstructionPushLiteralTestCase(unittest.TestCase):
-    pass
+    def test_push_literal_opcode_correct(self):
+        # setup
+        setup = _setup_process(
+            literals_content=[object_kinds.VM_Symbol("to_be_cloned", 0)],
+            stack_content=[None] * 4,
+            bytecode_content=[Opcodes.PUSH_LITERAL, 0x00],
+            none_object=None
+        )
+
+        # testing
+        process = object_kinds.VM_Process(None, setup.frame)
+        interpreter = Interpreter(UniverseMockup(), process)
+        interpreter.executeInstruction()
+
+        self.assertTrue(
+            setup.stack.item_get_at(0) == setup.literals.item_get_at(0),
+            "When push_literal opcode is executed, top of the stack should be copy of literal referenced by it"
+        )
 
 class InstructionPullTestCase(unittest.TestCase):
     pass
 
 class InstructionReturnExplicitTestCase(unittest.TestCase):
-    pass
+    def test_return_explicit_opcode_correct(self):
+        # setup for root frame
+        returned_object = object_kinds.VM_Symbol("to_be_returned", 0)
+
+        setup_for_root_frame = _setup_process(
+            literals_content=[],
+            stack_content=[None] * 4,
+            bytecode_content=[],
+            none_object=None
+        )
+
+        # setup for sub method
+        setup_for_sub_frame = _setup_process(
+            literals_content=[],
+            stack_content=[returned_object, None],
+            bytecode_content=[Opcodes.RETURN_EXPLICIT, 0x00],
+            none_object=None
+        )
+
+        # testing
+        process = object_kinds.VM_Process(None, setup_for_root_frame.frame)
+        process.push_frame(setup_for_sub_frame.frame)
+
+        interpreter = Interpreter(UniverseMockup(), process)
+        interpreter.executeInstruction()
+
+        self.assertTrue(
+            process.peek_frame() is setup_for_root_frame.frame,
+            "When return_explicit opcode is executed, the active frame must be the one that was before returning frame"
+        )
+
+        self.assertTrue(
+            setup_for_root_frame.stack.item_get_at(0) == returned_object,
+            "When return_explicit opcode is executed, the top of root stack must be the value returned from returned frame"
+        )
+
+    def test_return_explicit_root_correct(self):
+        # setup for root frame
+        returned_object = object_kinds.VM_Symbol("to_be_returned", 0)
+
+        setup_for_root_frame =_setup_process(
+            literals_content=[],
+            stack_content=[returned_object, None],
+            bytecode_content=[Opcodes.RETURN_EXPLICIT, 0x00],
+            none_object=None
+        )
+
+        # testing
+        process = object_kinds.VM_Process(None, setup_for_root_frame.frame)
+        interpreter = Interpreter(UniverseMockup(), process)
+        interpreter.executeInstruction()
+
+        self.assertTrue(
+            process.peek_frame() is not setup_for_root_frame.frame,
+            "When return_explicit opcode is executed in root frame, the active frame must not be the same as "
+        )
+
+        self.assertTrue(
+            process.get_ordinary_result() == returned_object,
+            "When return_explicit opcode is executed in root frame, the process result must be the return value from returning root frame"
+        )
 
 class InstructionSendTestCase(unittest.TestCase):
     pass
