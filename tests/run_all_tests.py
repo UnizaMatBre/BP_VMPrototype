6add7422369d37c0905e279f6d1627192b02acb9
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-04-12 13:04:37 +0200

    test: added runner that executes all tests

---
+++ tests/run_all_tests.py
@@ -0,0 +1,8 @@
+from tests.test_interpreter import *
+from tests.test_object_layout import *
+from tests.test_bytecode_parsing import *
+
+import unittest
+
+if __name__=="__main__":
+    unittest.main()
