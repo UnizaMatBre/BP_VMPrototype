cda7e0ed51d5fb7069e2d3dd152b0b9b499bed09
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-04-14 01:08:03 +0200

    feat: Added module and primitives for object array

---
+++ source/vm_core/primitives/primitives_object_array.py
@@ -0,0 +1,38 @@
+from source.vm_core.object_kinds import VM_ByteArray, VM_SmallInteger, VM_ObjectArray
+
+
+def primitive_object_array_item_get_at(interpreter, parameters):
+    object_array_object, index_object = parameters
+
+    assert isinstance(object_array_object, VM_ObjectArray)
+    assert isinstance(index_object, VM_SmallInteger)
+    assert 0 <= index_object.get_value() < object_array_object.get_item_count()
+
+    return interpreter.get_universe().new_small_integer(
+        object_array_object.item_get_at(index_object.get_value())
+    )
+
+def primitive_object_array_item_put_at(interpreter, parameters):
+    object_array_object, index_object, item_object = parameters
+
+    assert isinstance(object_array_object, VM_ObjectArray)
+    assert isinstance(index_object, VM_SmallInteger)
+    assert 0 <= index_object.get_value() < object_array_object.get_item_count()
+
+    object_array_object.item_put_at(index_object.get_code(), item_object)
+
+    return interpreter.get_universe().get_none_object()
+
+def primitive_byte_array_get_byte_count(interpreter, parameters):
+    object_array_object = parameters[0]
+
+    assert isinstance(object_array_object, VM_ObjectArray)
+
+    return interpreter.get_universe().new_small_integer(
+        object_array_object.get_item_count()
+    )
+
+
+LOCAL_PRIMITIVES = (
+
+)
