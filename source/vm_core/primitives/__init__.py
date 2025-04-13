caf94febc56091e82a736685fd7ce0f35dbc7aec
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-04-13 22:08:04 +0200

    feat: Implemented basic primitive loader

--- source/vm_core/primitives/__init__.py
+++ source/vm_core/primitives/__init__.py
@@ -1,4 +1,30 @@
+from source.vm_core.object_kinds import VM_PrimitiveMethod
+from source.vm_core.object_layout import SlotKind
+from source.vm_core.primitives import primitives_debug
 
 
-def add_primitives_into(primitives_holder):
-    pass
+
+def add_primitives_into(universe, primitives_holder):
+    """
+    Adds all primitives in package into specified object
+
+    :param universe: universe of object - needed to create symbols
+    :param primitives_holder: object where we want to insert out primitives
+    :return: None
+    """
+
+    def add_primitive(primitive_info):
+        primitive_name, primitive_param_count, primitive_func = primitive_info
+
+        symbol_name = universe.new_symbol(primitive_name, primitive_param_count)
+
+        primitives_holder.add_slot(
+            symbol_name,
+            SlotKind(),
+            VM_PrimitiveMethod(primitive_param_count, primitive_func)
+        )
+
+
+    # DEBUG PRIMITIVES
+    for one_primitive_info in primitives_debug.LOCAL_PRIMITIVES:
+        add_primitive(one_primitive_info)
