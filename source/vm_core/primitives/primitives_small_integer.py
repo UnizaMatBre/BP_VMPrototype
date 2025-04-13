42c43644c4d425ecec21f60b35c4cbd02c25a95f
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-04-14 00:36:47 +0200

    refactor: Moved common behavior into separate functon

--- source/vm_core/primitives/primitives_small_integer.py
+++ source/vm_core/primitives/primitives_small_integer.py
@@ -1,47 +1,38 @@
 from source.vm_core.object_kinds import VM_SmallInteger
 
 
-def primitive_small_integer_add(interpreter, parameters):
-    left_int, right_int = parameters
-
+def _do_arithmetic_operation(interpreter, left_int, right_int, operation):
     assert isinstance(left_int, VM_SmallInteger)
     assert isinstance(right_int, VM_SmallInteger)
 
     return interpreter.get_universe().new_small_integer(
-        left_int.get_value() + right_int.get_value()
+        operation(
+            left_int.get_code(),
+            right_int.get_value()
+        )
     )
+
+def primitive_small_integer_add(interpreter, parameters):
+    left_int, right_int = parameters
+
+    return _do_arithmetic_operation(interpreter, left_int, right_int, lambda a,b: a+b)
 
 
 def primitive_small_integer_sub(interpreter, parameters):
     left_int, right_int = parameters
 
-    assert isinstance(left_int, VM_SmallInteger)
-    assert isinstance(right_int, VM_SmallInteger)
-
-    return interpreter.get_universe().new_small_integer(
-        left_int.get_value() - right_int.get_value()
-    )
+    return _do_arithmetic_operation(interpreter, left_int, right_int, lambda a,b: a+b)
 
 
 def primitive_small_integer_mul(interpreter, parameters):
     left_int, right_int = parameters
 
-    assert isinstance(left_int, VM_SmallInteger)
-    assert isinstance(right_int, VM_SmallInteger)
-
-    return interpreter.get_universe().new_small_integer(
-        left_int.get_value() * right_int.get_value()
-    )
+    return _do_arithmetic_operation(interpreter, left_int, right_int, lambda a,b: a+b)
 
 def primitive_small_integer_div(interpreter, parameters):
     left_int, right_int = parameters
 
-    assert isinstance(left_int, VM_SmallInteger)
-    assert isinstance(right_int, VM_SmallInteger)
-
-    return interpreter.get_universe().new_small_integer(
-        int(left_int.get_value() / right_int.get_value())
-    )
+    return _do_arithmetic_operation(interpreter, left_int, right_int, lambda a,b: a+b)
 
 LOCAL_PRIMITIVES = (
 
